330ddf0fac57c69e9c95ed5219940e3946690b0d
Batsambuu Batbold <sambuu.coding@gmail.com>
2025-09-25 12:03:56 -0500

    DATA collected

--- test.cpp
+++ test.cpp
@@ -5,6 +5,9 @@
 #include <CGAL/Random.h>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef K::Point_3 Point_3;
@@ -13,21 +16,61 @@ typedef std::vector<Point_3> Points;
 
 int main()
 {
-  int n = 100;
-  Points points;
-
-  CGAL::Random rng;
-  CGAL::Random_points_in_cube_3<Point_3> gen(0.5, rng);
-
-  for (int i = 0; i < n; i++)
+  // Create output file in append mode to avoid overwriting
+  ofstream outputFile("convex_hull_results.txt", ios::app);
+  if (!outputFile.is_open())
   {
-    points.push_back(*gen++);
-    // printf("Point %d: (%f, %f, %f)\n", i, points[i].x(), points[i].y(), points[i].z());
+    cerr << "Error: Could not create output file!" << endl;
+    return 1;
   }
 
-  // Create a polyhedron to hold the convex hull
-  Polyhedron_3 hull_poly;
-  CGAL::convex_hull_3(points.begin(), points.end(), hull_poly);
+  // Write headers to both console and file
+  outputFile << "\n"
+             << string(50, '=') << endl;
+  outputFile << "3D Convex Hull Analysis Results for Sphere" << endl;
+  outputFile << "===============================" << endl;
+  time_t currentTime = time(nullptr);
+  outputFile << "Generated on: " << ctime(&currentTime) << endl;
 
-  std::cout << "Convex hull has " << hull_poly.size_of_vertices() << " vertices\n";
+  int runs = 10;
+
+  int arr[13] = {10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000};
+  for (auto n : arr)
+  {
+    outputFile << "\n"
+               << string(30, '-') << endl;
+    outputFile << "Number of points (n) = " << n << endl;
+    outputFile << "Number of runs = " << runs << endl;
+
+    // Seed the random number generator with current time to get different points each run
+    CGAL::Random rng(static_cast<unsigned int>(time(nullptr)));
+
+    for (int run = 1; run <= runs; run++)
+    {
+      Points points;
+      CGAL::Random_points_in_sphere_3<Point_3> gen(5, rng);
+
+      for (int i = 0; i < n; i++)
+      {
+        points.push_back(*gen++);
+        // Optionally write points to file (uncomment next line if you want all points)
+        // outputFile << "Point " << i << ": (" << points[i].x() << ", " << points[i].y() << ", " << points[i].z() << ")" << endl;
+      }
+
+      // Create a polyhedron to hold the convex hull
+      Polyhedron_3 hull_poly;
+      CGAL::convex_hull_3(points.begin(), points.end(), hull_poly);
+
+      // Output to both console and file
+      cout << "Run " << run << ": " << hull_poly.size_of_vertices() << " vertices" << endl;
+      outputFile << hull_poly.size_of_vertices() << endl;
+      points.clear();
+    }
+  }
+
+  // Close the file
+  outputFile.close();
+  cout << "\nResults appended to 'convex_hull_results.txt'" << endl;
+
+  return 0;
 }
